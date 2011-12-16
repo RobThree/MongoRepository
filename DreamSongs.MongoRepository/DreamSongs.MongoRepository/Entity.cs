000ba11cfbf17817cca635d4858e39afcb9fb01b
RobThree <rob@devcorner.nl>
2011-12-16 16:46:44 +0100

    Typo.

--- DreamSongs.MongoRepository/DreamSongs.MongoRepository/Entity.cs
+++ DreamSongs.MongoRepository/DreamSongs.MongoRepository/Entity.cs
@@ -12,7 +12,7 @@
         /// <summary>
         /// Gets or sets the id for this object (the primary record for an entity).
         /// </summary>
-        /// <value>Thet id for this object (the primary record for an entity).</value>
+        /// <value>The id for this object (the primary record for an entity).</value>
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
     }
