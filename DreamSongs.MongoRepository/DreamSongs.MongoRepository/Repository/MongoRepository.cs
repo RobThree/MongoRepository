4242c0daa3746ff0bd3d856d96a7a1ff07b248e3
RobThree <rob@devcorner.nl>
2011-12-16 12:13:25 +0100

    Turns out the RequestStart/RequestDone methods are "global" for the connectionpool; that simplified the implementation a helluva lot...

--- DreamSongs.MongoRepository/DreamSongs.MongoRepository/Repository/MongoRepository.cs
+++ DreamSongs.MongoRepository/DreamSongs.MongoRepository/Repository/MongoRepository.cs
@@ -196,6 +196,68 @@ namespace DreamSongs.MongoRepository
         {
             return _collection.AsQueryable().Any(criteria);
         }
+
+        /// <summary>
+        /// Lets the server know that this thread is about to begin a series of related operations that must all occur
+        /// on the same connection. The return value of this method implements IDisposable and can be placed in a using
+        /// statement (in which case RequestDone will be called automatically when leaving the using statement). 
+        /// </summary>
+        /// <returns>A helper object that implements IDisposable and calls RequestDone() from the Dispose method.</returns>
+        /// <remarks>
+        /// Sometimes a series of operations needs to be performed on the same connection in order to guarantee correct
+        /// results. This is rarely the case, and most of the time there is no need to call RequestStart/RequestDone.
+        /// An example of when this might be necessary is when a series of Inserts are called in rapid succession with
+        /// SafeMode off, and you want to query that data in a consistent manner immediately thereafter (with SafeMode
+        /// off the writes can queue up at the server and might not be immediately visible to other connections). Using
+        /// RequestStart you can force a query to be on the same connection as the writes, so the query won't execute
+        /// until the server has caught up with the writes.
+        /// A thread can temporarily reserve a connection from the connection pool by using RequestStart and
+        /// RequestDone. You are free to use any other databases as well during the request. RequestStart increments a
+        /// counter (for this thread) and RequestDone decrements the counter. The connection that was reserved is not
+        /// actually returned to the connection pool until the count reaches zero again. This means that calls to
+        /// RequestStart/RequestDone can be nested and the right thing will happen.
+        /// </remarks>
+        public IDisposable RequestStart()
+        {
+            return this.RequestStart(false);
+        }
+
+        /// <summary>
+        /// Lets the server know that this thread is about to begin a series of related operations that must all occur
+        /// on the same connection. The return value of this method implements IDisposable and can be placed in a using
+        /// statement (in which case RequestDone will be called automatically when leaving the using statement). 
+        /// </summary>
+        /// <returns>A helper object that implements IDisposable and calls RequestDone() from the Dispose method.</returns>
+        /// <param name="slaveOk">Whether queries should be sent to secondary servers.</param>
+        /// <remarks>
+        /// Sometimes a series of operations needs to be performed on the same connection in order to guarantee correct
+        /// results. This is rarely the case, and most of the time there is no need to call RequestStart/RequestDone.
+        /// An example of when this might be necessary is when a series of Inserts are called in rapid succession with
+        /// SafeMode off, and you want to query that data in a consistent manner immediately thereafter (with SafeMode
+        /// off the writes can queue up at the server and might not be immediately visible to other connections). Using
+        /// RequestStart you can force a query to be on the same connection as the writes, so the query won't execute
+        /// until the server has caught up with the writes.
+        /// A thread can temporarily reserve a connection from the connection pool by using RequestStart and
+        /// RequestDone. You are free to use any other databases as well during the request. RequestStart increments a
+        /// counter (for this thread) and RequestDone decrements the counter. The connection that was reserved is not
+        /// actually returned to the connection pool until the count reaches zero again. This means that calls to
+        /// RequestStart/RequestDone can be nested and the right thing will happen.
+        /// </remarks>
+        public IDisposable RequestStart(bool slaveOk)
+        {
+            return _collection.Database.RequestStart(slaveOk);
+        }
+
+        /// <summary>
+        /// Lets the server know that this thread is done with a series of related operations.
+        /// </summary>
+        /// <remarks>
+        /// Instead of calling this method it is better to put the return value of RequestStart in a using statement.
+        /// </remarks>
+        public void RequestDone()
+        {
+            _collection.Database.RequestDone();
+        }
     }
 }
 
