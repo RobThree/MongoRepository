6668a46eb72f430c438b4779dc961436cd91c5ee
RobThree <rob@devcorner.nl>
2013-04-10 21:00:34 +0200

    * Fix project version problems

--- MongoRepository/dev/MongoRepositoryTests/Properties/AssemblyInfo.cs
+++ MongoRepository/dev/MongoRepositoryTests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MongoRepository")]
-[assembly: AssemblyDescription("Repository Implementation for MongoDB C# Driver")]
+[assembly: AssemblyTitle("MongoRepositoryTests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DreamSongs. http://dreamsongs.se")]
-[assembly: AssemblyProduct("MongoRepository")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MongoRepositoryTests")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,16 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e3d1784-b1f3-4b33-a303-7facc780917e")]
+[assembly: Guid("6354bd62-0005-46c7-8412-213aaffbde48")]
 
-[assembly: AssemblyVersion("1.4.2.0")]
-[assembly: AssemblyFileVersion("1.4.2.0")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
