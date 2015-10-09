3dd9b8f36e69bc21ef3483b5ca8d530e501fe6ba
RobThree <rob@devcorner.nl>
2015-10-09 20:46:06 +0200

    * Updated to 1.10.1 driver

--- MongoRepository/Properties/AssemblyInfo.cs
+++ MongoRepository/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MongoRepository")]
 [assembly: AssemblyDescription("Repository Implementation for MongoDB C# Driver")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Devcorner.nl")]
 [assembly: AssemblyProduct("MongoRepository")]
-[assembly: AssemblyCopyright("Copyright © 2011 - 2015")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2015 Rob Janssen (RobIII)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7e3d1784-b1f3-4b33-a303-7facc780917e")]
 
-[assembly: AssemblyVersion("1.6.8.0")]
-[assembly: AssemblyFileVersion("1.6.8.0")]
+[assembly: AssemblyVersion("1.6.9.0")]
+[assembly: AssemblyFileVersion("1.6.9.0")]
